7f5db89ed447c6fab0df2deeea826f870e684cb5
PlugsBot <miniapp47@outlook.com>
2026-08-11 14:25:09 +0200

    new

--- script.js
+++ script.js
@@ -133,10 +133,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         { weight: '50G', price: 200.00 },
                         {
               weight: "100G",
-              price: 300.0,
-              oldPrice: 350.0,
-              badge: "PROMO ⚡️",
-            },
+              price: 350.0            },
                     ]
                 },
                 {
@@ -154,10 +151,8 @@ document.addEventListener('DOMContentLoaded', function () {
                         { weight: '50G', price: 200.00 },
                         {
               weight: "100G",
-              price: 300.0,
-              oldPrice: 350.0,
-              badge: "PROMO ⚡️",
-            },
+              price: 350.0            },
+            
                     ]
                 },
                    {
@@ -274,12 +269,10 @@ document.addEventListener('DOMContentLoaded', function () {
                         { weight: '9G', price: 50.00 },
                         { weight: '25G', price: 120.00 },
                         { weight: '50G', price: 200.00 },
-                        {
+                                             {
               weight: "100G",
-              price: 300.0,
-              oldPrice: 350.0,
-              badge: "PROMO ⚡️",
-            },
+              price: 350.0            },
+
                     ]
                 },
                 {
@@ -295,12 +288,10 @@ document.addEventListener('DOMContentLoaded', function () {
                         { weight: '9G', price: 50.00 },
                         { weight: '25G', price: 120.00 },
                         { weight: '50G', price: 200.00 },
-                        {
+                                          {
               weight: "100G",
-              price: 300.0,
-              oldPrice: 350.0,
-              badge: "PROMO ⚡️",
-            },
+              price: 350.0            },
+
                     ]
                 },
                 {
@@ -316,12 +307,10 @@ document.addEventListener('DOMContentLoaded', function () {
                         { weight: '9G', price: 50.00 },
                         { weight: '25G', price: 120.00 },
                         { weight: '50G', price: 200.00 },
-                        {
+                                        {
               weight: "100G",
-              price: 300.0,
-              oldPrice: 350.0,
-              badge: "PROMO ⚡️",
-            },
+              price: 350.0            },
+
                     ]
                 },
                 {
@@ -337,12 +326,10 @@ document.addEventListener('DOMContentLoaded', function () {
                         { weight: '9G', price: 50.00 },
                         { weight: '25G', price: 120.00 },
                         { weight: '50G', price: 200.00 },
-                        {
+                                               {
               weight: "100G",
-              price: 300.0,
-              oldPrice: 350.0,
-              badge: "PROMO ⚡️",
-            },
+              price: 350.0            },
+
                     ]
                 },
                 {
@@ -358,12 +345,10 @@ document.addEventListener('DOMContentLoaded', function () {
                         { weight: '9G', price: 50.00 },
                         { weight: '25G', price: 120.00 },
                         { weight: '50G', price: 200.00 },
-                        {
+                                               {
               weight: "100G",
-              price: 300.0,
-              oldPrice: 350.0,
-              badge: "PROMO ⚡️",
-            },
+              price: 350.0            },
+
                     ]
                 }
                   /* {
@@ -534,10 +519,10 @@ document.addEventListener('DOMContentLoaded', function () {
                     video: 'VideoSTP.mp4',
                     description: '⚡️🌸 STATIC SIFT PLASMA 🌸⚡️ \n\n Une exclusivité ultra premium signée 🌸 The Pink Farms 🌸. La Strawberry Pink est une extraction static d\'élite qui repousse les limites des profils fruités et floraux. Une véritable masterclass pour les palais les plus exigeants 🔥\n\n 👃 Aromatique :\n Dès l’ouverture, une explosion de fraise bien mûre et sucrée 🍓, enveloppée par des notes florales délicates 🌷. En arrière-plan, on retrouve un fond candy crémeux et une légère touche gazeuse ⛽️ qui vient rappeler l\'ADN US de cette génétique exceptionnelle.\n\n 👅 Goût :\n En bouche, c’est une expérience luxueuse : attaque gourmande sur la fraise bonbon, suivie d\'une rondeur lactée et florale qui tapisse le palais. La texture du static offre une combustion parfaite et une fumée épaisse, douce et ultra propre ☁️✨.\n\n 💨 Effets :\n Montée rapide avec un high très joyeux et euphorique 🚀. L\'esprit reste clair et créatif tandis qu\'une relaxation physique profonde s\'installe progressivement 😌. Idéal pour chiller avec une vibe ultra positive.\n\n 🏆 Le verdict :\n Un static sift full terpènes, à la texture sableuse et fondante. The Pink Farms livre ici un produit d\'une pureté maximale, alliant douceur extrême et vraie frappe de connaisseur 💎🍓.',
                     tarifs: [
-                                   { weight: "3G", price: 35.0 , oldPrice: 50.0, badge: "-30 ％", },
-            { weight: "10G", price: 98.0 , oldPrice: 140.0, badge: "-30 ％", },
-            { weight: "25G", price: 175.0 , oldPrice: 250.0, badge: "-30 ％", },
-            { weight: "50G", price: 315.0 , oldPrice: 450.0, badge: "-30 ％", },
+                                   { weight: "3G", price: 50.0 },
+            { weight: "10G", price: 140.0  },
+            { weight: "25G", price: 250.0 },
+            { weight: "50G", price: 450.0  },
                     ]
                 },
                 /*  {
